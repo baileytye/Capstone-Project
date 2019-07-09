d1406e4c2ae4a88f9a6172bfca86f4839d0a7686
Bailey Tye <bailey.s.tye@gmail.com>
2019-07-09 12:53:29 -0600

    Add empty 'AddProject' activity
     - Setup global toolbar used by all activities
     - Fixed some content of layouts
     - Fixed some styles of the toolbar to allow for center text titles

---
+++ app/src/main/java/com/bowtye/decisive/Activities/AddProjectActivity.java
@@ -0,0 +1,57 @@
+package com.bowtye.decisive.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.bowtye.decisive.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class AddProjectActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_project);
+        ButterKnife.bind(this);
+        prepareAppBar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.add_project, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NotNull MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finishAfterTransition();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void prepareAppBar() {
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Add Project");
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+}
