0468834d17ea277f624a2653a06ba7e39310d034
Bailey Tye <bailey.s.tye@gmail.com>
2019-07-10 17:44:21 -0600

    Create item card layout
     - Created layout for each item within a project
     - Cleaned up button code
     - Cleanud up add requirement card

--- app/src/main/java/com/bowtye/decisive/Activities/ProjectDetails.java
+++ app/src/main/java/com/bowtye/decisive/Activities/ProjectDetails.java
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
@@ -17,7 +16,7 @@ import java.util.Objects;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class AddProjectActivity extends AppCompatActivity {
+public class ProjectDetails extends AppCompatActivity {
 
     @BindView(R.id.toolbar) Toolbar mToolbar;
     @BindView(R.id.toolbar_title) TextView mToolbarTitle;
@@ -25,16 +24,9 @@ public class AddProjectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_project);
+        setContentView(R.layout.activity_project_details);
         ButterKnife.bind(this);
         prepareAppBar();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.add_project, menu);
-        return true;
     }
 
     @Override
@@ -52,7 +44,6 @@ public class AddProjectActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
-        mToolbarTitle.setText("Add Project");
-
+        mToolbarTitle.setText("PROJECT NAME");
     }
 }
